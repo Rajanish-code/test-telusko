9d44a194099328949174125bd3ef6fb9518fe5e3
Rajanish Kumar <Rajanishkmr49@gmail.com>
2026-09-20 15:29:32 +0530

    working in project

--- Main.java
+++ Main.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
+
 public class Main{
     public static void main(String[] args){
-        System.out.println("Hello, World!!");
+        Scanner Input = new Scanner(System.in);
+        System.out.println("Please enter the a and b number");
+        int a = Input.nextInt();
+        int b = Input.nextInt();
+        int c = a + b;
+        System.out.print(c);
     }
 }
